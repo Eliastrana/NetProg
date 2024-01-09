1266f5a9abbb96d56815536d9328ff8bc091d0eb
eliastt <eliastt@stud.ntnu.no>
2024-01-09 20:25:45 +0100

    made an internal readme

--- nettverksprog/oving1/cmake-build-debug/cmake_install.cmake
+++ nettverksprog/oving1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/eliastrana/CLionProjects/nettverksprog/oving1
+# Install script for directory: /Users/eliastrana/CLionProjects/nettverksprog/nettverksprog/oving1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/eliastrana/CLionProjects/nettverksprog/oving1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/eliastrana/CLionProjects/nettverksprog/nettverksprog/oving1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
